b5c0b60438b9606da2ac1808c2080f29285b7a4d
g-blok <gwblocher@gmail.com>
2023-11-06 21:03:40 -0800

    defined new app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AdventuresComponent } from './components/adventures/adventures.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ProjectsComponent,
+    AdventuresComponent,
+    ContactComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
