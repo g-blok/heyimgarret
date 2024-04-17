46c384cf408d0d93f26566ce2012eecc069a3c9c
g-blok <gwblocher@gmail.com>
2024-04-16 23:54:40 -0700

    updated WORK constants

--- src/app/interfaces/work.interface.ts
+++ src/app/interfaces/work.interface.ts
@@ -2,17 +2,16 @@ export interface WORK {
     company: string;
     displayName: string;
     jobTitle: string;
-    softwareBased: boolean;
     startDate:  string;
     endDate:  string;
     selected: boolean;
     overview?: string;
     achievements?: string[];
     technologies?: string[];
-    description?: WORK_DESCRIPTION[]
+    links?: WORK_LINKS[];
 }
 
-export interface WORK_DESCRIPTION {
-    task: string;
-    details: string[];
+export interface WORK_LINKS {
+    display: string;
+    url: string;
 }
