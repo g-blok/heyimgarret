fab74dabc105894cfe983b97a842723ba2e6b507
g-blok <gwblocher@gmail.com>
2024-10-10 17:55:23 -0700

    added small screen handler for footer contact section

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  isSmallScreen: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe('(max-width: 1264px)')
+      .subscribe((state: BreakpointState) => {
+        this.isSmallScreen = state.matches;
+      });
+  }
+}
